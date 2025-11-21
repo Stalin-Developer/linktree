163273efeaa5fcf6685b6e72249b15be9501cd53
Stalin Chávez <leonelstalin94@gmail.com>
2025-11-21 18:53:45 -0500

    We've prepared the project for a correct working with API's testers like postman and tunther client.

--- src/router.ts
+++ src/router.ts
@@ -11,6 +11,7 @@ const router = Router()
 //Routing
 router.get('/', (req, res) =>{
     console.log('Home')
+    res.send('Home funcionando 🎉');
 })
 /** Autenticacion y registro */
 router.post('/auth/register', (req, res) =>{
